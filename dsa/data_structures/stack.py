6fb5b8472a16af210fca883ee56d90e73604330f
Willen Leal <willenlealdev@gmail.com>
2024-06-29 20:58:17 -0400

    stack implementation

---
+++ dsa/data_structures/stack.py
@@ -0,0 +1,35 @@
+class Node[T]:
+    def __init__(self, val: T) -> None:
+        self.val = val
+        self.next: None | Node[T] = None
+
+
+class Stack[T]:
+    def __init__(self) -> None:
+        self.count = 0
+        self.head: Node[T] | None = None
+
+    def push(self, val: T) -> None:
+        self.count += 1
+        node = Node(val)
+
+        if not self.head:
+            self.head = node
+            return
+
+        node.next = self.head
+        self.head = node
+
+    def pop(self) -> T | None:
+        if not self.head:
+            return
+
+        self.count -= 1
+
+        head = self.head
+        self.head = self.head.next
+
+        return head.val
+
+    def peek(self) -> T | None:
+        return self.head.val if self.head else None
